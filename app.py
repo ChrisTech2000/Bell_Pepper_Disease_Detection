7efb870ef7af14e111715ef990ad4def9b016331
ChrisTech2000 <chrissolo2000@gmail.com>
2025-07-09 10:07:13 +0100

    Redirect to Virtual Agronomist page

--- app.py
+++ app.py
@@ -31,71 +31,98 @@ st.set_page_config(page_title="Manamuz Group", layout="centered")
 
 #success_holder = st.empty()
 
+#Defining session state tohandle navigation
+if "page" not in st.session_state:
+    st.session_state = "main"
 
-MODEL_PATH = "vgg19_model_2.h5"
-MODEL_URL = "https://drive.google.com/uc?id=1synPljrV3ooPhZJqHkO9oCu4wxi3KV_K"
-@st.cache_resource
 
-def load_model_from_drive():
-    if not os.path.exists(MODEL_PATH):
-        with st.spinner("Downloading model..."):
-            gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
-    return load_model(MODEL_PATH)
+#navigating to chat page
+if st.session_state.page == "chat":
+    st.title("Talk to Virtual Agronomist")
+    st.markdown("Ask your Question About your Bell Pepper Crop")
 
-#model = load_model_from_drive()
-#success_holder.success("Model Loaded Successfully")
-##time.sleep(0.5)
-#success_holder.empty()
+
+    if st.button("Back to App"):
+        st.session_state.page = "main"
+        st.rerun()
+
+
+else:
+    MODEL_PATH = "vgg19_model_2.h5"
+    MODEL_URL = "https://drive.google.com/uc?id=1synPljrV3ooPhZJqHkO9oCu4wxi3KV_K"
+    @st.cache_resource
+
+    def load_model_from_drive():
+        if not os.path.exists(MODEL_PATH):
+            with st.spinner("Downloading model..."):
+                gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
+        return load_model(MODEL_PATH)
+
 
 #Leting the load ed successfully show once
-if "model_loaded" not in st.session_state:
-    model = load_model_from_drive()
-    st.session_state.model_loaded = True
-    st.markdown(
-        "<h4 style='color:green;'>Model Loaded Successfully</h4>",
-        unsafe_allow_html=True
-    )
-else:
-    model = load_model(MODEL_PATH)
+    if "model_loaded" not in st.session_state:
+        model = load_model_from_drive()
+        st.session_state.model_loaded = True
+        st.markdown(
+            "<h4 style='color:green;'>Model Loaded Successfully</h4>",
+            unsafe_allow_html=True
+        ) 
+    else:
+        model = load_model(MODEL_PATH)
 
 
 
-#the header
-st.title("ManamuzGroup")
-st.markdown("Upload a bell pepper leaf image and click **Predict** to check if it's healthy or diseased.")
+    #the header
+    st.title("ManamuzGroup")
+    st.markdown("Upload a bell pepper leaf image and click **Predict** to check if it's healthy or diseased.")
 
-# Uploading the picture
-uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
+    # Uploading the picture
+    uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
 
-if uploaded_file:
-    image = Image.open(uploaded_file).convert('RGB')
+    if uploaded_file:
+        image = Image.open(uploaded_file).convert('RGB')
 
     # To make the user to view the uploaded image in a reduced size
-    display_image = image.resize((128, 128))
-    st.image(display_image, caption='Uploaded Bell_Pepper', use_container_width=False)
+        display_image = image.resize((128, 128))
+        st.image(display_image, caption='Uploaded Bell_Pepper', use_container_width=False)
 
     # Prediction section using the prediction button
-    if st.button("Predict"):
-        with st.spinner("Analyzing image..."):
-            img = image.resize((128, 128))
-            img_array = np.array(img) / 255.0
-            img_array = np.expand_dims(img_array, axis=0)
+        if st.button("Predict"):
+            with st.spinner("Analyzing image..."):
+                img = image.resize((128, 128))
+                img_array = np.array(img) / 255.0
+                img_array = np.expand_dims(img_array, axis=0)
 
             # Performing prediction using the model
-            prediction = model.predict(img_array)[0]
-            class_names = ["Healthy", "Unhealthy"]
-            pred_class = class_names[np.argmax(prediction)]
-            confidence = np.max(prediction) * 100
+                prediction = model.predict(img_array)[0]
+                class_names = ["Healthy", "Unhealthy"]
+                pred_class = class_names[np.argmax(prediction)]
+                confidence = np.max(prediction) * 100
+
+                #saving the state
+                st.session_state.predicted_class = pred_class
+                st.session_state.prediction_confidence = confidence
+
+                st.rerun()
+
+        if "predicted_class" in st.session_state:
+            pred_class = st.session_state.preicted_class
+            confidence = st.session_state.prediction_confidence
 
             # Displaying the prediction result
             st.markdown(f"### Prediction: **:red[{pred_class}]**")
             st.markdown(f"### Confidence: **{confidence:.2f}%**")
 
-            # Showing chatbot button only if the leaf is Unhealthy
             if pred_class == "Healthy":
-                st.markdown('You are Doing a Great Job Farmer! Keep it On!')
-            if pred_class == "Unhealthy":
-                if st.button("Chat with Virtual Agronomist"):
-                    st.info("Agronomist chat feature coming soon...")
+                st.markdown('You are Doing a Great Job, Farmer! Keep it On!')
+            elif pred_class == "Unhealthy":
+                st.markdown("Your Plant Appears Unhealthy")
+                time.sleep(1.5)
+
+                with st.spinner("Redirecting You to a Virtual Agronomist"):
+                    time.sleep(1.5)
+                    st.session_state.page = "chat"
+                    st.rerun()
+  
 
 
